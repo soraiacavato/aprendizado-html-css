7541f034855b4de9958f715523384c6e8d3acc55
Soraia Cavato <soraiacavato@gmail.com>
2026-01-10 23:41:30 -0300

    feat: add initial HTML structure, CSS styles, and JavaScript for theme toggle functionality

---
+++ 10_quarto_projeto/app.js
@@ -0,0 +1,21 @@
+// Selecionar elementos
+
+const root = document.documentElement;
+const btn = document.getElementById('btnTema');
+const ano = document.getElementById('ano');
+
+// Preenchendo ano atual
+
+ano.textContent = new Date().getFullYear();
+
+// Tema light/dark
+
+btn.addEventListener('click', () => {
+    const escuro = root.getAttribute('data-tema') === 'escuro'
+
+    if (escuro) {
+        root.removeAttribute('data-tema')
+    } else {
+        root.setAttribute('data-tema', 'escuro')
+    }
+})
